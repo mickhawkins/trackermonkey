8c2ba0b35b58490991b6d2ed8e032622334d461a
Mick Hawkins <37008548+mickhawkins@users.noreply.github.com>
2019-01-02 16:01:50 +0800

    Updated URL to remove search engine GET params

--- trackermonkey.js
+++ trackermonkey.js
@@ -4,8 +4,9 @@
 // @version      0.1
 // @description  Redirect MDL searches to Tracker
 // @author       Mick Hawkins
-// @include      /^https:\/\/(www\.)?duckduckgo\.com\/\?q=MDL-*/
-// @include      /^https:\/\/(www\.)?google\.com(\.[a-z]{2,})?\/(search)?\?(.*&)?q=MDL-*/
+// @source       https://github.com/mickhawkins/trackermonkey
+// @include      /^https:\/\/(www\.)?duckduckgo\.com\/\?q=MDL*/
+// @include      /^https:\/\/(www\.)?google\.com(\.[a-z]{2,})?\/(search)?\?(.*&)?q=MDL*/
 // @grant        none
 // ==/UserScript==
 
@@ -14,8 +15,19 @@
     window.stop();
 
     var url = window.location.href;
-    var patt = new RegExp("MDL-[0-9]+");
-    var mdl_ref = patt.exec(url);
+    var patt = /MDL([^&]*)(&.*)*/i;
+    var result = patt.exec(url);
+    var redirect = 'https://tracker.moodle.org/';
 
-    window.location = 'https://tracker.moodle.org/browse/' + mdl_ref;
+    if (result[1] != undefined) {
+        var search = result[1];
+
+        if (search.charAt(0) == '-') {
+            redirect += 'browse/MDL' + result[1];
+        } else if (search.indexOf('me') < 2) {
+            redirect += 'issues/?filter=-1';
+        }
+    }
+
+    window.location = redirect;
 })();
